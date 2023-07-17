0a010871ff37a77c2d5ebb0fc197c6748fce160c
Travis Nesland <tnesland@gmail.com>
2023-07-17 14:09:21 -0700

    Formatting fixes

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export type ActionResult<Output> = ActionResultHappy<Output> | ActionResultSad;
 
 export type ActionHandler<Context, Input, Output> = (
   ctx: Context & ActionBaseContext & { logger: Logger },
-  input: Input
+  input: Input,
 ) => Promise<Output> | Output;
 
 export interface ActionMetadata {
@@ -41,7 +41,7 @@ export interface ActionMetadata {
  */
 export function createAction<Context, Input, Output>(
   displayName: string,
-  handler: ActionHandler<Context, Input, Output>
+  handler: ActionHandler<Context, Input, Output>,
 ): ActionFactory<Context, Input, Output> {
   return new ActionFactory(displayName, handler);
 }
